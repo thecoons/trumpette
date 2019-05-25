55d044614dbe3618d472c95f21b381a8745d75ef
Barthelemy Antonin <antobarth@gmail.com>
2019-05-25 16:04:48 +0200

    Update main with Tweeter and Diode managers

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 import twitter
 import time
 import RPi.GPIO as GPIO
-from unittest import mock 
+from unittest import mock
+
+from module.tweeter.manager import TweeterManager
+from module.diode.manager import DiodeManager
 
 GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
+diode_manager = DiodeManager(interface=GPIO, pin=7)
 
 
 
@@ -16,11 +20,12 @@ api = twitter.Api(
     access_token_key='823823410350477313-4Fud2SRmY5J62sDkL8guCSFn8E8x7vU',
     access_token_secret='rI18QbDBN8j18MsFZTRf7eRPXoQQk4iufvCZVsD5vBYSI',
 )
+tweeter_manager = TweeterManager(api=api)
+
 last_tweet_id = -1
 
 while True:
-    trump_timeline = api.GetUserTimeline(user_id=25073877)
-    tweet = trump_timeline[0]
+    tweet = tweeter_manager.get_last_tweet(user_id=25073877)
 
     if last_tweet_id != tweet.id:
         last_tweet_text = tweet.text
@@ -28,9 +33,9 @@ while True:
         print(last_tweet_text)
         print(last_tweet_id)
         for tick in range(10):
-            GPIO.output(7, GPIO.HIGH)
+            diode_manager.turn_on()
             time.sleep(1)
-            GPIO.output(7, GPIO.LOW)
+            diode_manager.turn_off()
             time.sleep(1)
 
     time.sleep(300)
