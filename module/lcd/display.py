aebbb84f10c952d79f395157bd00f6a262f09ce3
Barthelemy Antonin <antobarth@gmail.com>
2019-05-30 12:50:43 +0200

    fix lcd size calculation

--- module/lcd/display.py
+++ module/lcd/display.py
@@ -10,7 +10,11 @@ class LCDDisplayManager:
         if self._frame_validation(frame):
             self.frame = frame
         else:
-            raise FrameTooLong('The frame you are tryng to set is not valid.')
+            raise FrameTooLong(
+                'The frame you are tryng to set is not valid.({})'.format(
+                    frame,
+                )
+            )
 
     def display_frame(self):
         self.lcd_interface.clear()
@@ -19,7 +23,7 @@ class LCDDisplayManager:
         )
 
     def lcd_char_size(self):
-        return self.lcd_interface["cols"] * self.lcd_interface["rows"]
+        return self.lcd_interface.lcd.cols * self.lcd_interface.lcd.rows
 
     def _frame_validation(self, frame):
         return len(frame) <= self.lcd_char_size()
--- tests/test_lcd.py
+++ tests/test_lcd.py
@@ -7,7 +7,8 @@ from module.lcd.exceptions import FrameTooLong
 class TestLCDModule(unittest.TestCase):
 
     def test_display_frame_has_been_flush_before_display_message(self):
-        lcd_interface = mock.Mock(rows=16, cols=16)
+        lcd_interface = mock.Mock()
+        lcd_interface.lcd = mock.Mock(rows=16, cols=16)
         frame = 'Hello Gary!'
         display_manager = LCDDisplayManager(
             lcd_interface,
@@ -20,7 +21,9 @@ class TestLCDModule(unittest.TestCase):
         lcd_interface.write_string.assert_called_once_with(frame)
 
     def test_frame_cant_have_more_char_than_display(self):
-        lcd_interface = mock.Mock(cols=16, rows=2)
+        lcd_interface = mock.Mock()
+        lcd_interface.lcd = mock.Mock(rows=2, cols=16)
+        
         frame = 'X' * ((16 * 2) + 1)
 
         with self.assertRaises(FrameTooLong):
